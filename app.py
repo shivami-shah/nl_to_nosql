f9b3fbb1716d918cc61a70695fcac60deea48fcf
shivami-shah <shivamishah@gmail.com>
2025-06-23 18:49:42 +0530

    set up gemini

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
